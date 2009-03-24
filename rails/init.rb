dfa5ed2bad133838c0f842892c4c665d26bd21be
Albert Callarisa <shark234@gmail.com>
2009-03-24 11:29:33 +0100

    added ubiquo_config inheritance

--- rails/init.rb
+++ rails/init.rb
@@ -1,6 +1,5 @@
 require 'ubiquo_authentication'
 
-default_per_page = Ubiquo::Config.get(:elements_per_page)
 Ubiquo::Plugin.register(:ubiquo_authentication, directory, config) do |config|
   config.add :remember_time, 2.weeks
   
@@ -11,7 +10,9 @@ Ubiquo::Plugin.register(:ubiquo_authentication, directory, config) do |config|
     access_control :DEFAULT => "ubiquo_user_management"
   }
   
-  config.add :ubiquo_users_elements_per_page, default_per_page
+  config.add :ubiquo_users_elements_per_page
+  config.add_inheritance :ubiquo_users_elements_per_page, :elements_per_page
+  
   config.add :ubiquo_users_admin_filter_enabled, true
 
   config.add :ubiquo_users_default_order_field, 'ubiquo_users.id'
