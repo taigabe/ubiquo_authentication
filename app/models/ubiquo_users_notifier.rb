39ad382ec708b88b2798106b0f45ed37b48c5283
Albert Callarisa <shark234@gmail.com>
2009-05-19 11:51:05 +0200

    localized users notification

--- app/models/ubiquo_users_notifier.rb
+++ app/models/ubiquo_users_notifier.rb
@@ -3,20 +3,22 @@ class UbiquoUsersNotifier < ActionMailer::Base
   #Send a mail to the desired user with a new generated password.
   #It's necesary to specify the host because mailer can't know in what host are running rails
   def forgot_password(user, host)
-    subject '[%s] Nueva contraseña generada' % Ubiquo::Config.get(:app_title)
+    subject I18n.t('ubiquo.auth.new_pass_generated', 
+      :app_title => Ubiquo::Config.get(:app_title), 
+      :locale => user.locale || Ubiquo.default_locale)
     recipients user.email
-    from Ubiquo::Config.get(:app_title) + "<railsmail@gnuine.com>"
-    @template += "_es"
+    from EMAIL_FROM
     body :user => user, :host => host
   end
   
   #Send a mail to the desired user with the information of their new account.
   #It's necesary to specify the host because mailer can't know in what host are running rails
   def confirm_creation(user, welcome_message, host)
-    subject '[%s] Nuevo usuario creado' % Ubiquo::Config.get(:app_title)
+    subject I18n.t('ubiquo.auth.new_user_created', 
+      :app_title => Ubiquo::Config.get(:app_title), 
+      :locale => user.locale || Ubiquo.default_locale)
     recipients user.email
-    from Ubiquo::Config.get(:app_title) + "<railsmail@gnuine.com>"
-    @template += "_es"
+    from EMAIL_FROM
     
     body :user => user, :host => host, :welcome_message => welcome_message
   end
