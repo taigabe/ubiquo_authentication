67c1d8e0b5481ce0dc8c36b7799152b63ca83eb9
Bernat Foj Capell <bfoj@gnuine.com>
2010-01-20 18:51:43 +0100

    Use default Ubiquo locale when sending welcome mails for new users [#210 state:resolved]

--- app/models/ubiquo_users_notifier.rb
+++ app/models/ubiquo_users_notifier.rb
@@ -14,13 +14,14 @@ class UbiquoUsersNotifier < ActionMailer::Base
   #Send a mail to the desired user with the information of their new account.
   #It's necesary to specify the host because mailer can't know in what host are running rails
   def confirm_creation(user, welcome_message, host)
+    locale = user.locale.blank? ? Ubiquo.default_locale : user.locale
     subject I18n.t('ubiquo.auth.new_user_created', 
       :app_title => Ubiquo::Config.get(:app_title), 
-      :locale => user.locale || Ubiquo.default_locale)
+      :locale => locale)
     recipients user.email
     from Ubiquo::Config.get(:notifier_email_from)
     
-    body :user => user, :host => host, :welcome_message => welcome_message
+    body :user => user, :host => host, :welcome_message => welcome_message, :locale => locale
   end
 
 end
