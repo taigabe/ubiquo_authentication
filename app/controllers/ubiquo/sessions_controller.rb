577e45bf04a12b81a4784e320519875fd7315acb
Toni Reina <areina0@gmail.com>
2010-01-22 12:07:36 +0100

    Updated indentation

--- app/controllers/ubiquo/sessions_controller.rb
+++ app/controllers/ubiquo/sessions_controller.rb
@@ -11,11 +11,15 @@ class Ubiquo::SessionsController < ApplicationController
 
   # login method. If OK, redirects to expected path or ubiquo_home.
   def create
-    self.current_ubiquo_user = UbiquoUser.authenticate(params[:login], params[:password])
+    self.current_ubiquo_user = UbiquoUser.authenticate(params[:login],
+                                                       params[:password])
     if logged_in?
       if params[:remember_me] == "1"
         self.current_ubiquo_user.remember_me
-        cookies[:auth_token] = { :value => self.current_ubiquo_user.remember_token , :expires => self.current_ubiquo_user.remember_token_expires_at }
+        cookies[:auth_token] = {
+          :value => self.current_ubiquo_user.remember_token ,
+          :expires => self.current_ubiquo_user.remember_token_expires_at
+        }
       end
       redirect_back_or_default(ubiquo_home_path)
       flash[:notice] = t 'ubiquo.auth.login_success'
@@ -25,7 +29,8 @@ class Ubiquo::SessionsController < ApplicationController
     end
   end
 
-  # logout method. Destroy all ubiquo_user data and send ubiquo_user to ubiquo_home (which commonly is login_required)
+  # logout method. Destroy all ubiquo_user data and send ubiquo_user
+  # to ubiquo_home (which commonly is login_required)
   def destroy
     self.current_ubiquo_user.forget_me if logged_in?
     cookies.delete :auth_token
