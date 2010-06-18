14f9317b76076b05d6c889af6d8df9f247b69c9e
Carlos Saura <csaura@gnuine.com>
2010-06-18 09:37:42 +0200

    Updated auth system and tests for new session[:ubiquo] [#315 state:resolved] (cherry picked from commit 6e970ec09c566621f6efc3d62a69006cb8d85d40)

--- test/functional/ubiquo/sessions_controller_test.rb
+++ test/functional/ubiquo/sessions_controller_test.rb
@@ -12,7 +12,7 @@ class Ubiquo::SessionsControllerTest < ActionController::TestCase
 
   def test_should_login_and_redirect
     post :create, :login => 'josep', :password => 'test'
-    assert session[:ubiquo_user_id]
+    assert session[:ubiquo][:ubiquo_user_id]
     assert_response :redirect
   end
   
@@ -20,7 +20,7 @@ class Ubiquo::SessionsControllerTest < ActionController::TestCase
     ubiquo_users(:josep).update_attribute(:is_active, false)
     
     post :create, :login => 'josep', :password => 'test'
-    assert session[:ubiquo_user_id].nil?
+    assert session[:ubiquo][:ubiquo_user_id].nil?
     assert_response :success
   end
 
@@ -29,7 +29,7 @@ class Ubiquo::SessionsControllerTest < ActionController::TestCase
   def test_should_be_able_to_login_when_first_user
     UbiquoUser.destroy_all
     post :create, :login => 'josep', :password => 'test'
-    assert session[:ubiquo_user_id]
+    assert session[:ubiquo][:ubiquo_user_id]
     assert_response :redirect
   end
 
@@ -37,20 +37,20 @@ class Ubiquo::SessionsControllerTest < ActionController::TestCase
     Rails.env.stubs(:production?).returns(true)
     UbiquoUser.destroy_all
     post :create, :login => 'josep', :password => 'test'
-    assert_nil session[:ubiquo_user_id]
+    assert_nil session[:ubiquo][:ubiquo_user_id]
     assert_response :success
   end
   
   def test_should_fail_login_and_not_redirect
     post :create, :login => 'josep', :password => 'bad password'
-    assert_nil session[:ubiquo_user_id]
+    assert_nil session[:ubiquo][:ubiquo_user_id]
     assert_response :success
   end
 
   def test_should_logout
     login_as :josep
     get :destroy
-    assert_nil session[:ubiquo_user_id]
+    assert_nil session[:ubiquo]
     assert_response :redirect
   end
 
@@ -86,7 +86,7 @@ class Ubiquo::SessionsControllerTest < ActionController::TestCase
   end
 
   def test_should_fail_expired_cookie_login
-    @request.session[:ubiquo_user_id] = nil #logout
+    @request.session.delete(:ubiquo) #logout
     ubiquo_users(:josep).remember_me
     ubiquo_users(:josep).update_attribute :remember_token_expires_at, 5.minutes.ago
     @request.cookies["auth_token"] = cookie_for(:josep)
@@ -95,7 +95,7 @@ class Ubiquo::SessionsControllerTest < ActionController::TestCase
   end
 
   def test_should_fail_cookie_login
-    @request.session[:ubiquo_user_id] = nil #logout
+    @request.session.delete(:ubiquo) #logout
     ubiquo_users(:josep).remember_me
     @request.cookies["auth_token"] = auth_token('invalid_auth_token')
     get :new
