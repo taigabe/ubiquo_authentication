d34c99ed26ac849f580937bc7c1a05b576f03442
Ramon Salvadó <rsalvado@gnuine.com>
2010-11-10 09:30:20 +0100

    Filter rewrite to conform to new API [#403 state:updated]

--- app/helpers/ubiquo/ubiquo_users_helper.rb
+++ app/helpers/ubiquo/ubiquo_users_helper.rb
@@ -1,50 +1,23 @@
 module Ubiquo::UbiquoUsersHelper
-  # returns the filter info for the active filters of ubiquo_users list
-  def ubiquo_users_filters_info(params)
-    filters = []
-    if Ubiquo::Config.context(:ubiquo_authentication).get(:ubiquo_users_string_filter_enabled)
-      filters << filter_info(:string, params,
-        :field => :filter_text,
-        :caption => t('ubiquo.text'))
-    end
-    if Ubiquo::Config.context(:ubiquo_authentication).get(:ubiquo_users_admin_filter_enabled)
-      filters << filter_info(:links, params,
-        :caption => t('ubiquo.auth.user_type'),
-        :boolean => true,    
-        :caption_true => t('ubiquo.auth.user_admin'),
+
+  def ubiquo_user_filters
+    filters_for 'UbiquoUser' do |f|
+      f.text if Ubiquo::Config.context(:ubiquo_authentication).get(:ubiquo_users_string_filter_enabled)
+      f.boolean(:admin, {
+        :caption       => t('ubiquo.auth.user_type'),
+        :caption_true  => t('ubiquo.auth.user_admin'),
         :caption_false => t('ubiquo.auth.user_non_admin'),
-        :field => :filter_admin)
+      }) if Ubiquo::Config.context(:ubiquo_authentication).get(:ubiquo_users_admin_filter_enabled)
     end
-    
-    build_filter_info(*filters)
   end
 
-  #return the enabled filters for ubiquo_users lists
-  def ubiquo_users_filters(url_for_options = {})
-    filters = []
-    if Ubiquo::Config.context(:ubiquo_authentication).get(:ubiquo_users_string_filter_enabled)
-      filters << render_filter(:string, url_for_options,
-        :field => :filter_text,
-        :caption => t('ubiquo.text'))
-    end
-    if Ubiquo::Config.context(:ubiquo_authentication).get(:ubiquo_users_admin_filter_enabled)
-      filters << render_filter(:links, url_for_options,
-        :caption => t('ubiquo.auth.user_type'),
-        :boolean => true,    
-        :caption_true => t('ubiquo.auth.user_admin'),
-        :caption_false => t('ubiquo.auth.user_non_admin'),
-        :field => :filter_admin)
-    end
-    filters.join
-  end
-  
   #renders the ubiquo_user list
   def ubiquo_user_list(collection, pages, options = {}, &block)
     concat render(:partial => "shared/ubiquo/lists/boxes", :locals => {
         :name => 'ubiquo_user',
-        :rows => collection.collect do |ubiquo_user| 
+        :rows => collection.collect do |ubiquo_user|
           {
-            :id => ubiquo_user.id, 
+            :id => ubiquo_user.id,
             :content => capture(ubiquo_user, &block),
             :actions => ubiquo_user_actions(ubiquo_user)
           }
@@ -54,14 +27,14 @@ module Ubiquo::UbiquoUsersHelper
                         new_ubiquo_ubiquo_user_path, :class => 'new')
       })
   end
-    
+
   private
-  
+
   #return the actios related to an ubiquo_user
   def ubiquo_user_actions(ubiquo_user, options = {})
     actions = []
     actions << link_to(t("ubiquo.edit"), [:edit, :ubiquo, ubiquo_user], :class => "edit")
-    actions << link_to(t("ubiquo.remove"), [:ubiquo, ubiquo_user], 
+    actions << link_to(t("ubiquo.remove"), [:ubiquo, ubiquo_user],
       :confirm => t("ubiquo.auth.confirm_user_removal"), :method => :delete, :class => "delete")
     actions
   end
