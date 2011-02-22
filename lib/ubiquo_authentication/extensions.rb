5428688115b1996f727fb462c416755673d8e170
Ramon Salvadó <rsalvado@gnuine.com>
2011-02-22 09:59:13 +0100

    Removed obsolete requires and autoload, load testcase improvements only when needed [#457 state:updated]

--- lib/ubiquo_authentication/extensions.rb
+++ lib/ubiquo_authentication/extensions.rb
@@ -1,12 +1,12 @@
 module UbiquoAuthentication
   module Extensions
-    autoload :Helper, 'ubiquo_authentication/extensions/helper'
-    autoload :TestCase, 'ubiquo_authentication/extensions/test_case'
-    autoload :Controller, 'ubiquo_authentication/extensions/controller'
   end
 end
 
 Ubiquo::Extensions::UbiquoAreaController.append_helper(UbiquoAuthentication::Extensions::Helper)
 Ubiquo::Extensions::UbiquoAreaController.append_include(UbiquoAuthentication::Extensions::Controller)
-ActionController::TestCase.send(:include, UbiquoAuthentication::Extensions::TestCase)
-ActionController::TestCase.setup(:login_as)
+
+if Rails.env.test?
+  ActionController::TestCase.send(:include, UbiquoAuthentication::Extensions::TestCase)
+  ActionController::TestCase.setup(:login_as)
+end
