ee3620477769b9580e733387f044dcf8dcf7023b
Toni Reina <areina0@gmail.com>
2012-01-20 11:10:11 +0100

    Unify migrations [#685]

--- install/db/migrate/20080710144713_create_ubiquo_users.rb
+++ install/db/migrate/20080710144713_create_ubiquo_users.rb
@@ -1,6 +1,8 @@
 class CreateUbiquoUsers < ActiveRecord::Migration
   def self.up
     create_table :ubiquo_users do |t|
+      t.string :name
+      t.string :surname
       t.string :login
       t.string :email
       t.boolean :is_admin
@@ -9,11 +11,13 @@ class CreateUbiquoUsers < ActiveRecord::Migration
       t.string :salt, :limit => 40
       t.string :remember_token
       t.datetime :remember_token_expires_at
-      
       t.string :photo_file_name
       t.integer :photo_file_size
       t.string :photo_content_type
-
+      t.boolean :is_superadmin, :default => true
+      t.string :locale
+      t.string :last_locale
+      
       t.timestamps
     end
     add_index :ubiquo_users, :login, :unique=>true
--- install/db/migrate/20101021092210_add_last_locale_to_ubiquo_user.rb
+++
@@ -1,9 +0,0 @@
-class AddLastLocaleToUbiquoUser < ActiveRecord::Migration
-  def self.up
-    add_column :ubiquo_users, :last_locale, :string
-  end
-
-  def self.down
-    remove_column :ubiquo_users, :last_locale
-  end
-end
