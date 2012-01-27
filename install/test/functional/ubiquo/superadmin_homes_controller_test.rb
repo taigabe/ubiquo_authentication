8fb22a2ee41bcda5edd8b9dede1131cd131771e7
Toni Reina <areina0@gmail.com>
2012-01-27 10:25:11 +0100

    Refactoring code [#685]

--- install/test/functional/ubiquo/superadmin_homes_controller_test.rb
+++ install/test/functional/ubiquo/superadmin_homes_controller_test.rb
@@ -1,23 +1,17 @@
 require File.dirname(__FILE__) + "/../../test_helper.rb"
 class Ubiquo::SuperadminHomesControllerTest < ActionController::TestCase
-  
-  test "should get show if superadmin" do
-    
-    user = UbiquoUser.find(login_as(:superadmin))
 
-    assert user.is_superadmin?
-    
+  test "should get show if superadmin" do
+    login(:admin)
+    assert assigns(:ubiquo_user).is_superadmin?
     get :show
-    
     assert_response :ok
   end
-  
+
   test "shouldnt get show if not superadmin" do
-    user = UbiquoUser.find(login_as(:eduard))
-    assert !user.is_superadmin?
-    
+    login(:eduard)
+    assert assigns(:ubiquo_user).is_superadmin?
     get :show
-    
-    assert_redirected_to ubiquo_login_path
+    assert_redirected_to ubiquo.login_path
   end
 end
--- lib/ubiquo_authentication/extensions.rb
+++ lib/ubiquo_authentication/extensions.rb
@@ -5,8 +5,3 @@ end
 
 Ubiquo::Extensions::Loader.append_helper(:UbiquoController, UbiquoAuthentication::Extensions::Helper)
 Ubiquo::Extensions::Loader.append_include(:UbiquoController, UbiquoAuthentication::Extensions::Controller)
-
-if Rails.env.test?
-  ActionController::TestCase.send(:include, UbiquoAuthentication::Extensions::TestCase)
-  ActionController::TestCase.setup(:login_as)
-end
