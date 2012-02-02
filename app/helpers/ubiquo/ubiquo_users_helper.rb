5c531a3842615d386d5618be7f9fe548a27e38ef
Toni Reina <areina0@gmail.com>
2012-02-02 18:20:19 +0100

    Updated ubiquo_users_list helper [#685]

--- app/helpers/ubiquo/ubiquo_users_helper.rb
+++ app/helpers/ubiquo/ubiquo_users_helper.rb
@@ -13,30 +13,31 @@ module Ubiquo::UbiquoUsersHelper
 
   #renders the ubiquo_user list
   def ubiquo_user_list(collection, pages, options = {}, &block)
-    concat render(:partial => "shared/ubiquo/lists/boxes", :locals => {
-        :name => 'ubiquo_user',
-        :rows => collection.collect do |ubiquo_user|
-          {
-            :id => ubiquo_user.id,
-            :content => capture(ubiquo_user, &block),
-            :actions => ubiquo_user_actions(ubiquo_user)
-          }
-        end,
-        :pages => pages,
-        :link_to_new => link_to(t("ubiquo.auth.new_user"),
-                        ubiquo.new_ubiquo_user_path, :class => 'new')
-      })
+    render(:partial => "shared/ubiquo/lists/boxes", :locals => {
+      :name => 'ubiquo_user',
+      :rows => collection.collect do |ubiquo_user|
+        {
+          :id => ubiquo_user.id,
+          :content => capture(ubiquo_user, &block),
+          :actions => ubiquo_user_actions(ubiquo_user)
+        }
+      end,
+      :pages => pages,
+      :link_to_new => link_to(t("ubiquo.auth.new_user"),
+                      ubiquo.new_ubiquo_user_path, :class => 'new')
+    })
   end
 
   private
 
-  #return the actios related to an ubiquo_user
+  #return the actions related to an ubiquo_user
   def ubiquo_user_actions(ubiquo_user, options = {})
-    actions = []
-    actions << link_to(t("ubiquo.edit"), ubiquo.edit_ubiquo_user_path(ubiquo_user), :class => "edit")
-    actions << link_to(t("ubiquo.remove"), ubiquo.ubiquo_user_path(ubiquo_user),
-      :confirm => t("ubiquo.auth.confirm_user_removal"), :method => :delete, :class => "delete")
-    actions
+    [
+      link_to(t("ubiquo.edit"), ubiquo.edit_ubiquo_user_path(ubiquo_user), :class => "btn-edit"),
+      link_to(t("ubiquo.remove"), ubiquo.ubiquo_user_path(ubiquo_user),
+        :confirm => t("ubiquo.auth.confirm_user_removal"),
+        :method => :delete, :class => "btn-delete")
+    ]
   end
 
 end
